1114daa5eed0ef1293eb8c590b54a6f098c48559
Brian Zerangue <brian.zerangue@gmail.com>
2013-02-02 09:49:47 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+servewestdallas.org
+===================
+
+ServeWestDallas
---
+++ assets/js/YouTube-poster-swapper.js
@@ -0,0 +1,7 @@
+﻿function viewTube(triggerID, movieURL, movieWth, movieHt) {
+    var embedcode = "<object width='" + movieWth + "&autostart=1' height='" + movieHt + "'>";
+    embedcode += "<param name='movie' value='/Script/" + movieURL + "'></param>";
+    embedcode += "<param name='wmode' value='transparent'></param>";
+    embedcode += "<embed src='/Script/" + movieURL + "&autoplay=1' type='application/x-shockwave-flash' wmode='transparent' width='" + movieWth + "' height='" + movieHt + "'></embed></object>";
+    document.getElementById(triggerID).innerHTML = embedcode;
+}
